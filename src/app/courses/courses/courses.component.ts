17d158c357dfa867154cc8f567b06185e7a0453a
Cris Jr <cristianoandre96@gmail.com>
2022-03-03 16:46:57 -0300

    Iniciando servico de cursos

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -8,16 +9,18 @@ import { Course } from '../model/course';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [
-    { _id: '1', name: 'Angular', category: 'front-end'}
-  ];
+  courses: Course[] = [];
   displayedColumns = ['name', 'category'];
 
-  constructor() {
+  // courseService: CoursesService;
+
+  constructor(private coursesService: CoursesService) {
     // this.courses = [];
+    // this.courseService = new CoursesService();
   }
 
   ngOnInit(): void {
+    this.courses = this.coursesService.list();
   }
 
 }
