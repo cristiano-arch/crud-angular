aac0851f71da7b47bb8d74918707d70bbacb6921
Cris Jr <cristianoandre96@gmail.com>
2022-03-08 09:51:52 -0300

    Configurando proxy

---
+++ proxy.conf.js
@@ -0,0 +1,14 @@
+const PROXY_CONFIG = [
+  {
+    context: ['/api'],
+    target: 'http://localhost:8080/',
+    secure: false,
+    loglevel: 'debug'
+  }
+];
+
+module.exports = PROXY_CONFIG;
+
+
+
+
