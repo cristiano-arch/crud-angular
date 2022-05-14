b73b443d0bea98d4c6baf4df3504610dbdd137a1
Cris Jr <cristianoandre96@gmail.com>
2022-05-14 16:36:26 -0300

    Criando select option de categoria

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-course-form',
@@ -10,22 +12,31 @@ export class CourseFormComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
-    this.form = this.formBuilder.group({
-      name: [null],
-      category: [null]
-    });
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private _snackBar: MatSnackBar) {
+      this.form = this.formBuilder.group({
+        name: [null],
+        category: [null]
+      });
   }
 
   ngOnInit(): void {
   }
 
   onSubmit() {
-
+    this.service.save(this.form.value).subscribe(
+      result => console.log(result),
+      error => this.onError()
+    );
   }
 
   onCancel() {
 
   }
 
+  private onError() {
+    this._snackBar.open('Erro ao salvar curso', '', { duration: 3000 });
+  }
 }
--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -8,6 +8,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   exports: [
@@ -19,7 +21,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatButtonModule,
     MatIconModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatSelectModule,
+    MatSnackBarModule
   ],
 })
 export class AppMaterialModule { }
