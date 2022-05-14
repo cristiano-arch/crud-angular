e399d352607baa175ffe266e464f4ff2bef7a262
Cris Jr <cristianoandre96@gmail.com>
2022-05-14 17:21:11 -0300

    Redirecionando para lista de cursos

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -15,7 +16,8 @@ export class CourseFormComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private service: CoursesService,
-    private _snackBar: MatSnackBar) {
+    private _snackBar: MatSnackBar,
+    private location: Location) {
       this.form = this.formBuilder.group({
         name: [null],
         category: [null]
@@ -27,16 +29,21 @@ export class CourseFormComponent implements OnInit {
 
   onSubmit() {
     this.service.save(this.form.value).subscribe(
-      result => console.log(result),
+      result => this.onSuccess(),
       error => this.onError()
     );
   }
 
   onCancel() {
+    this.location.back();
+  }
 
+  private onSuccess() {
+    this._snackBar.open('Curso salvo com sucesso!', '', { duration: 3000 });
+    this.onCancel();
   }
 
   private onError() {
-    this._snackBar.open('Erro ao salvar curso', '', { duration: 3000 });
+    this._snackBar.open('Erro ao salvar curso.', '', { duration: 3000 });
   }
 }
